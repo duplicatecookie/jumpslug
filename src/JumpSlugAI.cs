duplicatecookie/jumpslug

commit 1a4c82a8b6e1259426c28cdf1eab9bbdd068af83

exclude calls to FindPath from FollowPath from performance timings

src/JumpSlugAI.cs
@@ -196,13 +196,22 @@ class JumpSlugAI : ArtificialIntelligence {
                 }
                 path.Advance();
             }
-            path = destination is null
-                ? null
-                : pathfinder.FindPath(
+            if (destination is null) {
+                path = null;
+            } else if (Timers.active) {
+                Timers.followPath.Stop();
+                pathfinder.FindPath(
                     currentNode.gridPos,
                     destination.Value,
                     new SlugcatDescriptor(Player)
                 );
+            } else {
+                pathfinder.FindPath(
+                    currentNode.gridPos,
+                    destination.Value,
+                    new SlugcatDescriptor(Player)
+                );
+            }
             if (visualizer.visualizingPath) {
                 visualizer.TogglePath(path, new SlugcatDescriptor(Player));
                 if (path is not null) {

commit aebe82cb5d463c786b15f681ac3944ace3930062

fixed bug caused by 1a4c82a8b6e1259426c28cdf1eab9bbdd068af83 that resulted in the ai being unable to regenerate paths to the same destination

src/JumpSlugAI.cs
@@ -205,13 +205,14 @@ class JumpSlugAI : ArtificialIntelligence {
                 _path = null;
             } else if (Timers.Active) {
                 Timers.FollowPath.Stop();
-                _pathfinder.FindPath(
+                _path = _pathfinder.FindPath(
                     currentNode.GridPos,
                     _destination.Value,
                     new SlugcatDescriptor(Player)
                 );
+                Timers.FindPath.Start();
             } else {
-                _pathfinder.FindPath(
+                _path = _pathfinder.FindPath(
                     currentNode.GridPos,
                     _destination.Value,
                     new SlugcatDescriptor(Player)